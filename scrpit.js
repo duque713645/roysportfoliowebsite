58259e1643f086b367a5b7358b63dd0afed3ad3f
duque713645 <59882145+duque713645@users.noreply.github.com>
2022-04-18 22:05:50 -0500

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,8 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const nav = document.getElementsByClassName('nav')[0]
+
+toggleButton.addEventListener('click', () => {
+  nav.classList.toggle('active')
+})
+
+  
